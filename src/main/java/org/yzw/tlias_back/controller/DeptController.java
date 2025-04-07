61d419eafc46659a904894c99e2c05f6ec08f3f9
AlanYeager1 <hyrznasa198@163.com>
2025-04-07 17:45:55 +0800

    initial project structure

---
+++ src/main/java/org/yzw/tlias_back/controller/DeptController.java
@@ -0,0 +1,8 @@
+package org.yzw.tlias_back.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeptController {
+}
---
+++ src/main/java/org/yzw/tlias_back/service/Impl/DeptServiceImpl1.java
@@ -0,0 +1,8 @@
+package org.yzw.tlias_back.service.Impl;
+
+import org.springframework.stereotype.Service;
+import org.yzw.tlias_back.service.DeptService;
+
+@Service
+public class DeptServiceImpl1 implements DeptService {
+}
